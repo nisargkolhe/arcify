5e94eca7a0bd4ae6955e1e454ee834c72423e9db
Nisarg Kolhe <nisargkolhe@gmail.com>
2025-08-11 10:30:00 -0400

    fix clean button

--- sidebar.js
+++ sidebar.js
@@ -1310,7 +1310,10 @@ async function loadTabs(space, pinnedContainer, tempContainer) {
 function updateChevronState(spaceElement, pinnedContainer) {
     const chevronButton = spaceElement.querySelector('.space-toggle-chevron');
     const isCollapsed = pinnedContainer.classList.contains('collapsed');
-    
+    if (!chevronButton) {
+        return;
+    }
+
     if (isCollapsed) {
         chevronButton.classList.add('collapsed');
     } else {
