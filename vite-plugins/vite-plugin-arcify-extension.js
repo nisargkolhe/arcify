6b593e1c9d6febcc428ef08877d6e72e405f23db
Nisarg Kolhe <nisargkolhe@gmail.com>
2025-08-13 00:04:45 -0400

    installation onboarding

--- vite-plugins/vite-plugin-arcify-extension.js
+++ vite-plugins/vite-plugin-arcify-extension.js
@@ -11,11 +11,13 @@ function getExtensionInputs() {
     sidebar: resolve(process.cwd(), 'sidebar.html'),
     options: resolve(process.cwd(), 'options.html'),
     onboarding: resolve(process.cwd(), 'onboarding.html'),
+    'installation-onboarding': resolve(process.cwd(), 'installation-onboarding.html'),
     'spotlight-popup': resolve(process.cwd(), 'spotlight/popup.html'),
     background: resolve(process.cwd(), 'background.js'),
     'sidebar-script': resolve(process.cwd(), 'sidebar.js'),
     'options-script': resolve(process.cwd(), 'options.js'),
     'onboarding-script': resolve(process.cwd(), 'onboarding.js'),
+    'installation-onboarding-script': resolve(process.cwd(), 'installation-onboarding.js'),
     'spotlight-popup-script': resolve(process.cwd(), 'spotlight/popup.js'),
   };
 }
@@ -26,10 +28,12 @@ function getExtensionInputs() {
 function getExtensionOutput(isDev = false) {
   return {
     entryFileNames: (chunkInfo) => {
-      const mainScripts = ['background', 'sidebar-script', 'options-script', 'onboarding-script', 'spotlight-popup-script'];
+      const mainScripts = ['background', 'sidebar-script', 'options-script', 'onboarding-script', 'installation-onboarding-script', 'spotlight-popup-script'];
       if (mainScripts.includes(chunkInfo.name)) {
         if (chunkInfo.name === 'sidebar-script') return 'sidebar.js';
         if (chunkInfo.name === 'options-script') return 'options.js';
+        if (chunkInfo.name === 'onboarding-script') return 'onboarding.js';
+        if (chunkInfo.name === 'installation-onboarding-script') return 'installation-onboarding.js';
         if (chunkInfo.name === 'spotlight-popup-script') return 'spotlight/popup.js';
         return `${chunkInfo.name}.js`;
       }
