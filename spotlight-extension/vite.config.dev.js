ed01f176bfa930190e7a1c02149e08db66126265
Chinmay Phulse <phulsechinmay@Chinmays-MacBook-Air.local>
2026-02-03 17:41:14 -0800

    deleted spotlight extension folder

--- spotlight-extension/vite.config.dev.js
+++
@@ -1,4 +0,0 @@
-import { defineConfig } from 'vite';
-import { createSpotlightConfig } from './vite-plugins/vite-plugin-spotlight-extension.js';
-
-export default defineConfig(createSpotlightConfig({ isDev: true }));
